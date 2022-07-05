167350983631a375f6642fbfe402e3e88c5e8daa
liuzhaozhi <1765805596@qq.com>
2022-07-05 10:04:00 +0800

    feat: 创建 state - createStore

---
+++ src/redux/languageReducer.ts
@@ -0,0 +1,19 @@
+interface LanguageState {
+  language: string
+  languageList: { name: string, code: string }[]
+}
+
+const defaultState: LanguageState = {
+  language: 'zh',
+  languageList: [{
+    name: '中文',
+    code: 'zh'
+  }, {
+    name: 'English',
+    code: 'en'
+  }]
+}
+
+export default (state = defaultState, action) => {
+  return state
+}
