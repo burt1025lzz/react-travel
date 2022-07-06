2c747af83f2a7d9d74cd4a36a5c2d789292caca7
liuzhaozhi <1765805596@qq.com>
2022-07-06 09:12:44 +0800

    feat: 合作企业模块添加多语言支持

--- src/components/businessPartners/BusinessPartners.tsx
+++ src/components/businessPartners/BusinessPartners.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Row, Col, Typography, Divider } from "antd";
+import {Row, Col, Typography, Divider} from "antd";
 import styles from './BusinessPartners.module.scss';
 import image1 from '../../assets/images/microsoft-80658_640.png';
 import image2 from '../../assets/images/icon-720944_640.png';
 import image3 from '../../assets/images/follow-826033_640.png';
 import image4 from '../../assets/images/facebook-807588_640.png';
+import {useTranslation} from "react-i18next";
 
 const companies = [
-    { src: image1, title: "Microsoft"},
-    { src: image2, title: "Youtube"},
-    { src: image3, title: "Ins"},
-    { src: image4, title: "Facebook"}
+  {src: image1, title: "Microsoft"},
+  {src: image2, title: "Youtube"},
+  {src: image3, title: "Ins"},
+  {src: image4, title: "Facebook"}
 ]
-
 export const BusinessPartners: React.FC = () => {
+  const {t} = useTranslation()
+
   return (
     <div className={styles.content}>
       <Divider orientation="left">
-        <Typography.Title level={3}>合作企业</Typography.Title>
+        <Typography.Title level={3}>{t('home_page.joint_venture')}</Typography.Title>
       </Divider>
       <Row>
         {companies.map((c, index) => (
