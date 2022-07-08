70de1cae35da1d78a67e90f26b8802cf19750cfe
liuzhaozhi <1765805596@qq.com>
2022-07-08 10:31:55 +0800

    feat: Antd + ts 表单处理

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {UserLayout} from "../../layout";
+import {RegisterForm} from "./RegisterForm";
 
 export const RegisterPage: React.FC = () => {
   return (
     <>
       <UserLayout>
-        <h1>注册页面</h1>
+        <RegisterForm/>
       </UserLayout>
     </>
   )
